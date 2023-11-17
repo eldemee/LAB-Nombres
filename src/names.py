6eeef0deae1bdcc9d3caccc4a409f075c778ec78
eldemee <demetrio.lop3zlanzas@gmail.com>
2023-11-17 17:19:09 +0100

    actu

--- src/names.py
+++ src/names.py
@@ -1,5 +1,7 @@
 import csv
-from typing import NamedTuple, List, Optional, Set
+from typing import NamedTuple, List, Optional, Set, Tuple, Dict
+from collections import defaultdict
+import matplotlib.pyplot as plt
 
 NameFreq = NamedTuple("NameFreq",[("year",int),("name",str),("frequency", int),("gender",str)])
 
@@ -24,5 +26,47 @@ def get_names(names: List[NameFreq], g:Optional[str]=None) -> Set[str]:
     
 #EXERCISE 4
 def got_top_n_year(names: List[NameFreq], year: int, limit:int=10, g:Optional[str]=None) -> List[Tuple[str,int]]:
-    
+    #SELECT TUPLES ACCORDING TO YEAR AND GENDER
+    selection = [s for s in names if s.year == year and (g==None or s.gender==g)]
+    #SORT TUPLES ACCORDING TO FREQUENCY
+    selection.sort(key=lambda a:a.frequency, reverse=True)
+    #CREATE LIST OF TUPLES OF THE FIRST LIMIT
+    #return selection[:limit]
+    return [(s.name, s.frequency) for s in selection][:limit]
 
+#EX 5
+def get_common_names(names: List[NameFreq]) -> Set[str]:
+    man_names = {s.name for s in names if s.gender=="Hombre"}
+    woman_names = {s.name for s in names if s.gender=="Mujer"}
+    return man_names & woman_names
+
+#EX 6
+def get_compound_names(names:List[NameFreq], g:Optional[str]=None) -> Set[str]:
+    return {s.name for s in names if ' ' in s.name and (g==None or g==s.gender)}
+
+#EX 7
+def most_commons_names_per_year(names:List[NameFreq]):
+    dic = defaultdict(list)
+    for s in names:
+        year = s.year
+        dic[year].append((s.name, s.frequency))
+    orden = sorted(dic.items(),key=lambda x:x[1][1][1], reverse=True)
+    for s in orden:
+        print(s[0], s[1][1][0])
+    return orden
+
+
+#EX 8
+def get_year_frequencies(names: List[NameFreq], name: str) -> List[Tuple[int,int]]:
+    d = defaultdict(int)
+    for s in names:
+        if s.name == name:
+            d[s.year] += s.frequency
+    return sorted(d.items())
+
+
+def show_evolution_year(names: List[NameFreq], name:str) ->None:
+    year, freq = zip(*get_year_frequencies(names, name))
+    plt.plot(year, freq)
+    plt.title(f"Evolición nombre {name}")
+    plt.show()
--- src/names_test.py
+++ src/names_test.py
@@ -9,7 +9,14 @@ def main():
     print(f"Hay una frecuencia de {len(male_freqs)} nombres")
     name_woman = get_names(names_freq, "Mujer")
     print(f"Hay {len(name_woman)}")
+    freq_name_per_year = got_top_n_year(names_freq, 2017)
+    print(freq_name_per_year)
+    selec = get_common_names(names_freq)
+    print(selec)
 
+    print(get_compound_names(names_freq))
+    most_commons_names_per_year(names_freq)
+    show_evolution_year(names_freq, "HUGO")
 
 if __name__ == "__main__":
     main()
